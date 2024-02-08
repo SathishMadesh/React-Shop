f7d0ce24e02188debf3071cd938980dd0fc93d57
SathishMadesh <s747272946@gmail.com>
2024-02-08 17:00:10 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div class="container">
-          <a class="navbar-brand" href="#">MW Collection</a>
+          <a class="navbar-brand fw-bold fs-4" href="#">MW Collection</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -26,7 +26,13 @@ export default function Navbar() {
             </ul>
             <div className="buttons">
               <a href="" className="btn btn-outline-dark">
-                <i className="fa fa-sign-in me-1">Login</i>
+                <i className="fa fa-sign-in me-2">Login</i>
+              </a>
+              <a href="" className="btn btn-outline-dark ms-2">
+                <i className="fa fa-user-plus me-2">Register</i>
+              </a>
+              <a href="" className="btn btn-outline-dark ms-2">
+                <i className="fa fa-shopping-cart me-2">Cart (0)</i>
               </a>
             </div>
           </div>
