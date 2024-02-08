812e7accfdae636fc1e94d591158a3ccac4b93ef
SathishMadesh <s747272946@gmail.com>
2024-02-08 16:33:00 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,10 +24,9 @@ export default function Navbar() {
                 <a class="nav-link" href="#">Contact</a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div className="buttons">
+              <a href="" className='btn btn-outline-dark'></a>
+            </div>
           </div>
         </div>
       </nav>
