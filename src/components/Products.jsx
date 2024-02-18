31e32f0289cec782fd6bc323de74999fec7f9ea1
SathishMadesh <s747272946@gmail.com>
2024-02-18 23:07:31 +0530

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -45,13 +45,13 @@ export default function Products() {
                 {filter.map((product) => {
                     return (
                         <>
-                            <div className="col-md-3">
-                                <div className="card">
-                                    <img src={product.image} class="card-img-top" alt={product.title} />
+                            <div className="col-md-3 mb-4">
+                                <div className="card h-100 text-center p-4" key={product.id}>
+                                    <img src={product.image} class="card-img-top" alt={product.title} height="250px"/>
                                     <div className="card-body">
-                                        <h5 className="card-title">{product.title}</h5>
-                                        <p className="card-text">${product.price}</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        <h5 className="card-title mb-0">{product.title.substring(0,12)}...</h5>
+                                        <p className="card-text lead fw-bold">${product.price}</p>
+                                        <a href="#" className="btn btn-outline-dark">Buy Now</a>
                                     </div>
                                 </div>
                             </div>
