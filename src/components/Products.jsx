1e906174fa10c561ca5a5b21a40d305561df07ac
SathishMadesh <s747272946@gmail.com>
2024-02-19 20:08:10 +0530

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -49,10 +49,10 @@ export default function Products() {
             <>
                 <div className="buttons d-flex justify-content-center mb-5 pb-5">
                     <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>All</button>
-                    <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>Men's Clothing</button>
-                    <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>Women's Clothing</button>
-                    <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>Jewelery</button>
-                    <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>Electronic</button>
+                    <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("men's clothing")}>Men's Clothing</button>
+                    <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("women's clothing")}>Women's Clothing</button>
+                    <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("jewelery")}>Jewelery</button>
+                    <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct("electronics")}>Electronic</button>
                 </div>
                 {filter.map((product) => {
                     return (
