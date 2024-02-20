4a9b1e8a1a764896097fcd1c1735ae7b7b735a91
SathishMadesh <s747272946@gmail.com>
2024-02-20 16:52:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import {Switch, Route} from 'react-router-dom';
+import Products from './components/Products';
 
 function App() {
   return (
     <div>
-     <Navbar/> 
-     <Home/>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" Component={Home}/> 
+        <Route exact path="/products" Component={Products}/>
+      </Switch>
     </div>
   );
 }
