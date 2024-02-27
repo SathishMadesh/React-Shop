3743fcd2781cbefb5892f0ec2ae81360ee6b637a
SathishMadesh <s747272946@gmail.com>
2024-02-27 16:37:25 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
 
 export default function Product() {
 
@@ -41,6 +41,14 @@ export default function Product() {
                         Rating {product.rating && product.rating.rate}
                         <i className="fa fa-star"></i>
                     </p>
+                    <h3 className="display-6 fw-bold my-4">
+                        $ {product.price}
+                    </h3>
+                    <p className="lead">{product.description}</p>
+                    <button className="btn btn-outline-dark px-4 py-2">
+                        Add to Cart
+                    </button>
+                    <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2"> Go to Cart </NavLink>
                 </div>
             </>
         )
@@ -48,8 +56,8 @@ export default function Product() {
 
     return (
         <div>
-        <div className="container">
-            <div className="row">
+        <div className="container py-5">
+            <div className="row py-4">
                 {loading? <Loading/> : <ShowProduct/>}
             </div>
         </div>
